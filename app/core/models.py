fa7fb5a6306be8d40c8ca699ead5a798ee447ed4
WailBenoulha <wailbnlh@gmail.com>
2024-12-26 13:15:21 -1100

    setup media config and model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class ImageUpload(models.Model):
+    title = models.CharField(max_length = 255)
+    image = models.ImageField(upload_to='images',blank=True,null=True)
+
+    def __str__(self):
+        return self.title
